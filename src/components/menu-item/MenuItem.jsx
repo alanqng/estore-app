db3fce7fb5afa4017f2ac387983d6b350795ea09
Alan Ng <alanq.ng@gmail.com>
2019-08-07 22:08:27 +0200

    Add react router and update hat components and routes

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import './menu-item.scss'
+import React from 'react';
+import './menu-item.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title, img, size}) => {
-    return (
-        <div className={`${size} menu-item`}>
-        <div className='background-image' style={{backgroundImage: `url(${img})`}}/>  
-        <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-    )
-}
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
+  return (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
 
-export default MenuItem
+export default withRouter(MenuItem);
