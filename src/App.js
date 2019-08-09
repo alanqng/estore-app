a48a3e325d42cfdceb8dc44018d537f7204cbe94
Alan Ng <alanq.ng@gmail.com>
2019-08-09 20:12:15 +0200

    Integrated firebase and authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import { Route, Switch } from 'react-router-dom'
 import Shop from './pages/shop/Shop';
 import './App.css';
 import Header from './components/header/Header';
 import SigninSignup from './pages/signin-signup/SigninSignup';
-
+import { auth } from './firebase/firebase.utils'
 function App() {
+  const [ currentUser, setCurrentUser ] = useState(null);
+  
+  useEffect(() => {
+    let unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+      console.log(currentUser)
+    } )
+    return () => {
+      unsubscribeFromAuth()
+    };
+  }, [currentUser])
   return (
     <div className="App">
-    <Header/>
+    <Header currentUser={currentUser}/>
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={Shop}/>
--- src/components/signin/Signin.jsx
+++ src/components/signin/Signin.jsx
@@ -43,9 +43,11 @@ export default class Signin extends Component {
             label="Password"
             required
           />
-
-          <CustomButton type="submit"> Submit </CustomButton>
-          <CustomButton onClick={signInWithGoogle}> Sign in with Google </CustomButton>
+          <div className='buttons'>
+            <CustomButton type="submit"> Submit </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+          </div>
+          
         </form>
       </div>
     );
