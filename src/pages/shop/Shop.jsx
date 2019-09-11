63d0a71027230bdaffbb15c69209239f23ebaba4
Alan Ng <alanq.ng@gmail.com>
2019-09-11 20:26:40 +0200

    Added asyn fetch for collections

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,37 +1,49 @@
 import React, {useEffect, useState} from 'react'
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
 import { Route } from 'react-router-dom'
-import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils'
 import CollectionPage from '../collection/Collection'
 import WithSpinner from '../../components/with-spinner/WithSpinner'
 import { connect } from 'react-redux'
-import { updateCollections } from '../../redux/shop/shop.actions'
-
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
+import { createStructuredSelector } from 'reselect'
+import { selectIsCollectionFetching, selectIsCollectionLoaded } from '../../redux/shop/shop.selectors'
 const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview)
 const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
-const Shop =  ({ match, updateCollections }) => {
-    let unsubscribeFromsnapshot = null;
-    const [loading, setLoading] = useState(true)
+const Shop =  ({ match, fetchCollectionAsync, isCollectionLoaded }) => {
 
     useEffect(() => {
-        const collectionRef = firestore.collection('collections')
-        unsubscribeFromsnapshot = collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionSnapshotToMap(snapshot)
-            updateCollections(collectionsMap)
-            setLoading(false)
-        })
+        fetchCollectionAsync()
+        // const collectionRef = firestore.collection('collections')
+        
+        // collectionRef.get().then(snapshot => {
+        //     const collectionMap = convertCollectionSnapshotToMap(snapshot)
+        //     updateCollections(collectionMap)
+        //     setLoading(false)
+        // })
+        
+        // unsubscribeFromsnapshot = collectionRef.onSnapshot(async snapshot => {
+        //     const collectionsMap = convertCollectionSnapshotToMap(snapshot)
+        //     updateCollections(collectionsMap)
+        //     setLoading(false)
+        // })
     }, [])
     return (
         <div className='shop-page'>
-            <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props}/>} />
-            <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
+            <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={!isCollectionLoaded} {...props}/>} />
+            <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionLoaded} {...props} />} />
         </div>
         )
 }
 
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+    fetchCollectionAsync: () => dispatch(fetchCollectionsStartAsync())
 })
 
-export default connect(null, mapDispatchToProps)(Shop)
+const mapStateToProps = createStructuredSelector({
+    isLoading: selectIsCollectionFetching,
+    isCollectionLoaded: selectIsCollectionLoaded
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Shop)
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,29 @@
 import ShopActionTypes from './shop.types'
+import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils'
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = (collectionsMap) => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START,
+})
+
+export const fecthCollectionsSuccess = (collectionsMap) => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap
 })
+
+export const fecthCollectionsFailure = (error) => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: error
+})
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections')
+        dispatch(fetchCollectionsStart())
+
+        collectionRef.get().then(snapshot => {
+            const collectionMap = convertCollectionSnapshotToMap(snapshot)
+            dispatch(fecthCollectionsSuccess(collectionMap))
+        }).catch(error => dispatch(fecthCollectionsFailure(error)))
+        
+    }
+}
